f88ef9dbe2c3a816df520e0a299c83268aadd64f
evan <jeremilafarge@protonmail.com>
2025-02-10 20:23:44 +0100

    🐛: method create db instance added

--- src/classes/db_manager.py
+++ src/classes/db_manager.py
@@ -10,8 +10,12 @@ class DatabaseManager:
             cls._instance = super().__new__(cls)
             base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
             full_path = os.path.join(base_dir, db_path)
+
             if not os.path.exists(full_path):
-                raise FileNotFoundError(f"Le fichier de base de données est introuvable : {full_path}")
+                print(f"Database not found, creating on {full_path}...")
+                os.makedirs(os.path.dirname(full_path), exist_ok=True)
+                cls._create_database(full_path)
+
             cls._instance.conn = sqlite3.connect(full_path, check_same_thread=False)
             cls._instance.cursor = cls._instance.conn.cursor()
         return cls._instance
@@ -30,3 +34,10 @@ class DatabaseManager:
 
     def close(self):
         self.conn.close()
+
+    @staticmethod
+    def _create_database(db_path):
+        conn = sqlite3.connect(db_path)
+        conn.commit()
+        conn.close()
+        print("Database created.")
